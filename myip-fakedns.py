67190a89860bd4f7beae62f568d780233b7f933c
Daniel Perna <danielperna84@gmail.com>
2017-02-17 22:40:57 +0100

    Update myip-fakedns.py

--- myip-fakedns.py
+++ myip-fakedns.py
@@ -3,27 +3,27 @@ import socket
 
 class DNSQuery:
     def __init__(self, data):
-        self.data=data
-        self .domain=''
+        self.data = data
+        self.domain = ''
 
         tipo = (ord(data[2]) >> 3) & 15   # Opcode bits
         if tipo == 0:                     # Standard query
-            ini=12
-            lon=ord(data[ini])
+            ini = 12
+            lon = ord(data[ini])
             while lon != 0:
-                self.domain+=data[ini+1:ini+lon+1]+'.'
-                ini+=lon+1
-                lon=ord(data[ini])
+                self.domain += data[ini+1:ini+lon+1]+'.'
+                ini += lon+1
+                lon = ord(data[ini])
 
     def answer(self, ip):
-        packet=''
+        packet = ''
         if self.domain:
-            packet+=self.data[:2] + "\x81\x80"
-            packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
-            packet+=self.data[12:]                                         # Original Domain Name Question
-            packet+='\xc0\x0c'                                             # Pointer to domain name
-            packet+='\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
-            packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
+            packet += self.data[:2] + "\x81\x80"
+            packet += self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
+            packet += self.data[12:]                                         # Original Domain Name Question
+            packet += '\xc0\x0c'                                             # Pointer to domain name
+            packet += '\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
+            packet += str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
         return packet
 
 if __name__ == '__main__':
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     try:
         while 1:
             data, addr = udps.recvfrom(1024)
-            p=DNSQuery(data)
+            p = DNSQuery(data)
             udps.sendto(p.answer(addr[0]), addr)
             print 'Answer: %s -> %s' % (p.domain, addr[0])
     except KeyboardInterrupt:
